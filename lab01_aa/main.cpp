cb38c4489a23ef405e975eaf2c61c405d431bdd9
dotruger <dotruger.37@gmail.com>
2019-09-11 22:59:18 +0300

    One word

--- lab01_aa/main.cpp
+++ lab01_aa/main.cpp
@@ -1,18 +1,18 @@
 //Complect MinGW 32bits
 
-//"sorsktdfkjjk" "kosrtfdjkkj" - It is death for recursion method
+//"sorsktdfkjjk" "kosrtfdjkkj" - It is like death for recursion method
 
 #include "alg_laewenstein_.h"
 
 int main()
 {
-    string s1 = "hello";
-    string s2 = "halo";
+    string s1 = "sorskt";
+    string s2 = "kosrt";
 
-    cout << levenstein(s1, s2) << endl;
+    //cout << levenstein(s1, s2) << endl;
     cout << levenstein_rec(s1, s2) << endl;
-    cout << dameray_levenstein(s1, s2) << endl;
-    cout << dameray_levenstein_rec(s1, s2) << endl;
+    //cout << dameray_levenstein(s1, s2) << endl;
+    //cout << dameray_levenstein_rec(s1, s2) << endl;
 
     cout <<"\nprogramm completed\n";
     return 0;
