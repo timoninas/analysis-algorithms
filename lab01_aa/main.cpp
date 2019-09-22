e21e42444f93bb43fe3fcf57663d6bbf60da83cd
dotruger <dotruger.37@gmail.com>
2019-09-22 12:49:11 +0300

    add lib ctime

--- lab01_aa/main.cpp
+++ lab01_aa/main.cpp
@@ -3,16 +3,24 @@
 //"sorsktdfkjjk" "kosrtfdjkkj" - It is like death for recursion method
 
 #include "alg_laewenstein_.h"
+#include <ctime>
 
 int main()
 {
-    string s1 = "sorskt";
-    string s2 = "kosrt";
+    string s1 = "sorsktdfkjjk";
+    string s2 = "kosrtfdjkkj";
 
-    cout << levenstein(s1, s2) << endl;
+    unsigned int start_time = clock();
+
+    //cout << levenstein(s1, s2) << endl;
     cout << levenstein_rec(s1, s2) << endl;
-    cout << dameray_levenstein(s1, s2) << endl;
-    cout << dameray_levenstein_rec(s1, s2) << endl;
+    //cout << dameray_levenstein(s1, s2) << endl;
+    //cout << dameray_levenstein_rec(s1, s2) << endl;
+
+    unsigned int end_time = clock();
+
+    unsigned int search_time = end_time - start_time;
+    cout << search_time / 1000.0 << endl;
 
     cout <<"\nprogramm completed\n";
     return 0;
